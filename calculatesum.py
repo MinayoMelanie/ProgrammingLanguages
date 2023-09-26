8f51572474cc75583adf20e8652f8233870ecb9a
MinayoMelanie <melanieminayo00@gmail.com>
2023-09-26 13:14:22 +0300

    Making a calculator for getting sum

---
+++ calculatesum.py
@@ -0,0 +1,9 @@
+name = input("Enter your name:")
+print("Hello "+name)
+num1= input("Input num1: ")
+num2 = input("Input num2: ")
+
+num1=int(num1)
+num2=int(num2)
+
+print("The sum of the two numbers is : ",num1 + num2)
